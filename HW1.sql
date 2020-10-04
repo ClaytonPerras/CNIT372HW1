3346fb23a2474527d07bee2f3ef3773d073f794d
ClaytonPerras <59108696+ClaytonPerras@users.noreply.github.com>
2020-10-04 17:45:46 -0400

    Update HW1.sql

--- HW1.sql
+++ HW1.sql
@@ -60,7 +60,7 @@ BEGIN
                 IF count2 = usr_input THEN
                     dbms_output.put_line(count1 * count2);
                 ELSE
-                    dbms_output.put(rpad(count1 * count2, 4));
+                    dbms_output.put(rpad(count1 * count2, 5));
                 END IF;
                 count2 := count2 + 1;
                 EXIT WHEN count2 > usr_input;
@@ -75,5 +75,37 @@ begin
     generate_multiplication_table_simple(10);
 end;
 /*
+QUESTION 2a
+Procedure GENERATE_MULTIPLICATION_TABLE_FOR compiled
 
+1    2    3    4    5    6    7    8    9    10   
+2    4    6    8    10   12   14   16   18   20   
+3    6    9    12   15   18   21   24   27   30   
+4    8    12   16   20   24   28   32   36   40   
+5    10   15   20   25   30   35   40   45   50   
+6    12   18   24   30   36   42   48   54   60   
+7    14   21   28   35   42   49   56   63   70   
+8    16   24   32   40   48   56   64   72   80   
+9    18   27   36   45   54   63   72   81   90   
+10   20   30   40   50   60   70   80   90   100   
+
+
+PL/SQL procedure successfully completed.
+
+QUESTION 2c
+Procedure GENERATE_MULTIPLICATION_TABLE_SIMPLE compiled
+
+1    2    3    4    5    6    7    8    9    10
+2    4    6    8    10   12   14   16   18   20
+3    6    9    12   15   18   21   24   27   30
+4    8    12   16   20   24   28   32   36   40
+5    10   15   20   25   30   35   40   45   50
+6    12   18   24   30   36   42   48   54   60
+7    14   21   28   35   42   49   56   63   70
+8    16   24   32   40   48   56   64   72   80
+9    18   27   36   45   54   63   72   81   90
+10   20   30   40   50   60   70   80   90   100
+
+
+PL/SQL procedure successfully completed.
 */
